8ebf3c5ced5e37377a55974c74347e49063928ee
Yuniarsiti <141316968+Yuniarsiti@users.noreply.github.com>
2025-04-24 16:07:45 +0700

    Add files via upload

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -17,7 +17,7 @@ const setActive = (tab) => {
 <template>
     <nav v-if="route?.name != 'login'" class="navbar navbar-expand-lg">
         <div class="container">
-            <img src="../assets/img/profile.png" alt="logo" class="logo">
+            <img src="../assets/img/logo.png" alt="logo" class="logo">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
